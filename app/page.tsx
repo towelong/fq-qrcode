41240233e60486b0971f0b2350c895c084699f16
towelong <towelong@qq.com>
2024-11-12 21:55:35 +0800

    fix: logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import EntryInput from '@/components/EntryInput'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+import EntryInput from "@/components/EntryInput";
+// import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const token = cookies().get('token')
-  if (token) {
-    redirect('/subscribe')
-  }
+  // const token = cookies().get('token')
+  // if (token) {
+  redirect("/subscribe");
+  // }
   return (
     <main>
       <EntryInput />
     </main>
-  )
+  );
 }
--- app/subscribe/getSubscribe.ts
+++ app/subscribe/getSubscribe.ts
@@ -1,31 +1,41 @@
-import { Buffer } from 'buffer'
+import { Buffer } from "buffer";
 
 export interface ISubscribe {
-  name: string
-  url: string
+  name: string;
+  url: string;
 }
 
 export async function getSubscribe() {
-  const res = await fetch(process.env.SUBSCRIBE_URL!, { next: { revalidate: 60 } })
-  const str = await res.text()
-  const decodedData = Buffer.from(str, 'base64')
-  const listText = decodedData.toString('utf-8')
-  const rows: ISubscribe[] = []
-  listText.split('\r\n').forEach((url) => {
-    const name = getTypeFromUrl(url)
-    if (url !== '' && name.includes('专线')) {
+  const res = await fetch(process.env.SUBSCRIBE_URL!, {
+    next: { revalidate: 60 },
+  });
+  const str = await res.text();
+  const decodedData = Buffer.from(str, "base64");
+  const listText = decodedData.toString("utf-8");
+  console.log(listText);
+  const rows: ISubscribe[] = [];
+  listText.split("\r\n").forEach((url) => {
+    const name = getTypeFromUrl(url);
+    if (
+      url !== "" &&
+      name.indexOf("剩余") < 0 &&
+      name.indexOf("套餐") < 0 &&
+      name.indexOf("距离") < 0
+    ) {
       rows.push({
         url,
         name,
-      })
+      });
     }
-  })
-  return rows
+  });
+  console.log(rows);
+  return rows;
 }
 
 function getTypeFromUrl(url: string): string {
   // 解码整个URL
-  const decodedURL = decodeURIComponent(url)
-  const decoded = decodedURL.split('#')
-  return decoded.length === 2 ? decoded[1] : ''
+  const decodedURL = decodeURIComponent(url);
+  console.log(decodedURL);
+  const decoded = decodedURL.split("#");
+  return decoded.length === 2 ? decoded[1] : "";
 }
