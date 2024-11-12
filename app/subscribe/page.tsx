b7ae9d78ddd290ba08516df36fff954b5361786c
towelong <towelong@qq.com>
2024-11-12 21:27:53 +0800

    feat: 去除校验

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,13 +1,13 @@
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
+// import { redirect } from 'next/navigation'
+// import { cookies } from 'next/headers'
 import { getSubscribe } from './getSubscribe'
 import SubscribeList from './SubscribeList'
 
 export default async function Subscribe() {
-  const cookiesList = cookies()
-  if (!cookiesList.has('token')) {
-    redirect('/')
-  }
+  // const cookiesList = cookies()
+  // if (!cookiesList.has('token')) {
+  //   redirect('/')
+  // }
   const rows = await getSubscribe()
 
   return (
